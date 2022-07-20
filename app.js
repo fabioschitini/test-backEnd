f6af6e67f86528d62c04fe0c5bae3707ef570cf7
Fabio Schitini <fabioschitini1@hotmail.com>
2022-07-20 11:21:48 -0300

    .

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(session({
   secret: 'street',
   resave: true,
